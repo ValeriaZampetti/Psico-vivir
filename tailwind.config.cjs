84d12c90aeffe085dcf3c2426d3f0f646bc83b90
Sergionx <sergio180203@gmail.com>
2023-02-11 16:44:22 -0400

    added colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFEAEC",
+        secondary: "#CBFFC9",
+      }
+    },
   },
   plugins: [],
 } 
