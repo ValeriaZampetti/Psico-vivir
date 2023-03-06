936b2f85640d932b167383a9db3be28683329977
Sergionx <sergio180203@gmail.com>
2023-03-06 16:31:24 -0400

    using context

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { Footer } from "../components/navigation/Footer";
 import LandingHeader from "../components/navigation/LandingHeader";
+import AuthProvider from "../context/AuthProvider";
 import { useAuth } from "../hooks/useAuth";
 
 function ProtectedLayout() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
-  if (!user) {
+  // TODO - Crear componente para cargar
+  if (loading) {
     return (
       <>
+        <AuthProvider>
+          <h1>Loading</h1>
+        </AuthProvider>
+      </>
+    );
+  }
+
+  if (!user) return <Navigate to="users/login" />;
+
+  console.log(user);
+
+  return (
+    <>
+      <AuthProvider>
         <div>
           <LandingHeader />
           <div className="bg-white min-[768px]:max-[868px]:mt-[6rem] mt-[4.5rem]">
@@ -17,19 +33,7 @@ function ProtectedLayout() {
           </div>
           <Footer />
         </div>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <div>
-        <LandingHeader />
-        <div className="bg-white min-[768px]:max-[868px]:mt-[6rem] mt-[4.5rem]">
-          <Outlet />
-        </div>
-        <Footer />
-      </div>
+      </AuthProvider>
     </>
   );
 }
