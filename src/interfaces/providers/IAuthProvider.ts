9073ce4652acf62e466d69dff6599becbf4509ad
Sergionx <sergio180203@gmail.com>
2023-03-06 16:31:00 -0400

    added login and loading

--- src/interfaces/providers/IAuthProvider.ts
+++ src/interfaces/providers/IAuthProvider.ts
@@ -1,6 +1,8 @@
-import { Client } from "../Client";
+import { UserCredential } from "firebase/auth";
+import { Client, Doctor } from "../Client";
 
 export interface IAuthProvider {
-  user: Client | null;
-  setUser: (user: Client | null) => void;
+  user: Client | Doctor  | null;
+  loading: boolean
+  login: (email: string, password: string) => Promise<UserCredential>;
 }
