4f0fadcecf96a0a0c9dfe37cfa6f20f71b9c93ef
Sergionx <sergio180203@gmail.com>
2023-03-06 16:30:54 -0400

    added login and loading

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,11 @@
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  UserCredential,
+} from "firebase/auth";
 import React, { createContext, useEffect, useState } from "react";
+import { getUserById } from "../firebase/api";
+import { auth } from "../firebase/config";
 import { Client } from "../interfaces/Client";
 import { IAuthProvider } from "../interfaces/providers/IAuthProvider";
 
@@ -8,17 +15,39 @@ interface IProps {
 
 export const AuthContext = createContext<IAuthProvider>({
   user: null,
-  setUser: () => {},
+  loading: true,
+  login: () => new Promise(() => {}),
 });
 
 function AuthProvider({ children }: IProps) {
   const [user, setUser] = useState<Client | null>(null);
-  const value: IAuthProvider = {
-    user: user,
-    setUser: setUser,
-  };
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const unsubuscribe = onAuthStateChanged(auth, async (currentUser) => {
+      console.log({ currentUser });
+      setUser(await getUserById(currentUser?.uid || ""));
+      console.log(user);
+      setLoading(false);
+    });
+
+    console.log(loading);
+    return () => unsubuscribe();
+  }, []);
+
+  async function login(
+    email: string,
+    password: string
+  ): Promise<UserCredential> {
+    console.log("login", email, password);
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  const value: IAuthProvider = {
+    user,
+    loading,
+    login,
+  };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
