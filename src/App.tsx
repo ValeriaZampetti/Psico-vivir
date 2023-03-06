206a218f99aa09187aec035fe1b470f07b5e4888
Sergionx <sergio180203@gmail.com>
2023-03-06 16:32:29 -0400

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LandingLayout from "./layouts/LandingLayout";
+import ProtectedLayout from "./layouts/ProtectedLayout";
 import Error404 from "./pages/Error404";
 import Landing from "./pages/Landing";
 import Chat from "./pages/psico/Chat";
@@ -12,35 +11,35 @@ import Login from "./pages/users/Login";
 import Register from "./pages/users/Register";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="*" element={<LandingLayout />}>
-                    <Route index element={<Landing />} />
-                </Route>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<LandingLayout />}>
+          <Route index element={<Landing />} />
+        </Route>
 
-                <Route path="/landing" element={<LandingLayout />}>
-                    <Route index element={<Landing />} />
-                </Route>
+        <Route path="/landing" element={<LandingLayout />}>
+          <Route index element={<Landing />} />
+        </Route>
 
-                <Route path="/users" element={<LandingLayout />}>
-                    <Route index element={<Login />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                </Route>
+        <Route path="/users" element={<LandingLayout />}>
+          <Route index element={<Navigate to="login" />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
 
-                <Route path="/psico" element={<LandingLayout />}>
-                    <Route index element={<Searcher />} />
-                    <Route path="profile/:id" element={<Profile />} />
-                    <Route path="schedule" element={<Schedule />} />
-                    <Route path="chat" element={<Chat />} />
-                </Route>
-                <Route path="*" element={<LandingLayout />}>
-                    <Route index element={<Error404 />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/psico" element={<ProtectedLayout />}>
+          <Route index element={<Searcher />} />
+          <Route path="profile/:id" element={<Profile />} />
+          <Route path="schedule" element={<Schedule />} />
+          <Route path="chat" element={<Chat />} />
+        </Route>
+        <Route path="*" element={<LandingLayout />}>
+          <Route index element={<Error404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
