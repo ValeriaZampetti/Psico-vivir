dc06e9c789446bd598d4411d7bd6b2e2cf3ba09f
Sergionx <sergio180203@gmail.com>
2023-03-08 20:51:37 -0400

    better buttton

--- src/pages/users/Login.tsx
+++ src/pages/users/Login.tsx
@@ -10,116 +10,115 @@ import { useAuth } from "../../hooks/useAuth";
 //import Swal from "sweetalert2";
 
 export const LogIn = (props: any) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const { login } = useAuth();
-    const navigate = useNavigate();
-    const notUser = () => {
-        //Swal.fire({
-        //icon: "error",
-        //title: "Ha ocurrido un error",
-        //text: "El usuario no está registrado, por favor registrese antes de iniciar sesión",
-        //});
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const notUser = () => {
+    //Swal.fire({
+    //icon: "error",
+    //title: "Ha ocurrido un error",
+    //text: "El usuario no está registrado, por favor registrese antes de iniciar sesión",
+    //});
+  };
 
-    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-        try {
-            const userCredential = await login(email, password);
+    try {
+      const userCredential = await login(email, password);
 
-            if (userCredential) {
-                setTimeout(() => {
-                    // TODO - Avisar que sera redirigido
-                    console.log("me voy");
-                    navigate("/psico");
-                }, 2000);
-            } else {
-                console.log("no user");
-            }
-        } catch (error) {
-            console.log(error);
-        }
+      if (userCredential) {
+        setTimeout(() => {
+          // TODO - Avisar que sera redirigido
+          console.log("me voy");
+          navigate("/psico");
+        }, 2000);
+      } else {
+        console.log("no user");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return (
-        <>
-            <div className="bg-gray-300/40 px-14 py-7">
-                <div
-                    className="backdrop-blur-lg bg-white  drop-shadow-lg
-            flex flex-row p-6 rounded-2xl justify-center"
+  return (
+    <>
+      <div className="bg-gray-300/40 px-14 py-7">
+        <div
+          className="backdrop-blur-lg bg-white drop-shadow-lg
+                      flex flex-row p-6 rounded-2xl justify-center"
+        >
+          <div className="w-[50%] lg:flex hidden justify-center">
+            <img
+              src={psicoLogin}
+              className="w-auto h-[37rem] rounded-2xl bg-primary-normal"
+            />
+          </div>
+          <main className="flex flex-col  justify-center lg:w-[50%]">
+            <p className="text-end mb-12  ">
+              ¿No tienes una cuenta?{" "}
+              <Link
+                to="/users/register"
+                className="text-blue-500 hover:text-blue-700 cursor-pointer"
+              >
+                Registrate
+              </Link>
+            </p>
+            <h1 className="text-2xl font-bold text-center">Bienvenido!</h1>
+            <h2 className="text-center text-xl font-medium">
+              Preparado para el cambio?
+            </h2>
+            <form
+              className="my-10 flex flex-col justify-center gap-5 w-64 self-center"
+              onSubmit={handleSubmit}
+            >
+              <input
+                className="rounded-lg p-4 border-2 border-primary-strong"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <input
+                className="rounded-lg p-4 border-2 border-primary-strong"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+              <p>Olvidaste tu contraseña?</p>
+
+              <button
+                type="submit"
+                className="w-full py-3 text-black font-bold rounded-lg shadow-lg
+                bg-primary-light hover:bg-primary-normal hover:scale-95 active:scale-90
+                  hover:ring-4 ring-primary-strong ring-offset-2 ring-offset-gray-100"
+              >
+                INICIA SESIÓN
+              </button>
+            </form>
+
+            <footer className="mb-5">
+              <p className="text-center">O inicia sesión con</p>
+              <div className="flex justify-center gap-5 mt-5">
+                <button
+                  className="bg-white hover:bg-gray-100 active:ring-1 ring-black font-bold py-2 px-4 rounded-full 
+                drop-shadow-md hover:drop-shadow-lg"
                 >
-                    <div className="w-[50%] lg:flex hidden justify-center">
-                        <img
-                            src={psicoLogin}
-                            className="w-auto h-[37rem] rounded-2xl bg-primary-normal"
-                        />
-                    </div>
-                    <main className="flex flex-col  justify-center lg:w-[50%]">
-                        <p className="text-end mb-12  ">
-                            ¿No tienes una cuenta?{" "}
-                            <Link
-                                to="/users/register"
-                                className="text-blue-500 hover:text-blue-700 cursor-pointer"
-                            >
-                                Registrate
-                            </Link>
-                        </p>
-                        <h1 className="text-2xl font-bold text-center">
-                            Bienvenido!
-                        </h1>
-                        <h2 className="text-center text-xl font-medium">
-                            Preparado para el cambio?
-                        </h2>
-                        <form
-                            className="my-10 flex flex-col justify-center gap-5 w-64 self-center"
-                            onSubmit={handleSubmit}
-                        >
-                            <input
-                                className="rounded-lg p-4 border-2 border-primary-strong"
-                                placeholder="Email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            ></input>
-                            <input
-                                className="rounded-lg p-4 border-2 border-primary-strong"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            ></input>
-                            <p>Olvidaste tu contraseña?</p>
-
-                            {/* TODO - Cambiar bg-green */}
-                            <input
-                                type="submit"
-                                value="Iniciar Sesión"
-                                className="w-full py-3 text-black font-bold uppercase rounded  transition-colors mb-5 
-                  bg-primary-light hover:bg-primary-normal active:ring-1 ring-black shadow-lg cursor-pointer"
-                            />
-                        </form>
-
-                        <footer className="mb-5">
-                            <p className="text-center">O inicia sesión con</p>
-                            <div className="flex justify-center gap-5 mt-5">
-                                <button
-                                    className="bg-white hover:bg-gray-100 active:ring-1 ring-black font-bold py-2 px-4 rounded-full 
+                  <img src={facebookIcon} />
+                </button>
+                <button
+                  className="bg-white hover:bg-gray-100 active:ring-1 ring-black font-bold py-2 px-4 rounded-full 
                 drop-shadow-md hover:drop-shadow-lg"
-                                >
-                                    <img src={facebookIcon} />
-                                </button>
-                                <button
-                                    className="bg-white hover:bg-gray-100 active:ring-1 ring-black font-bold py-2 px-4 rounded-full 
-                drop-shadow-md hover:drop-shadow-lg"
-                                >
-                                    <img src={googleIcon} />
-                                </button>
-                            </div>
-                        </footer>
-                    </main>
-                </div>
-            </div>
-        </>
-    );
+                >
+                  <img src={googleIcon} />
+                </button>
+              </div>
+            </footer>
+          </main>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LogIn;
