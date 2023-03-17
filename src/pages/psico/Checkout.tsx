9b20b6317f99fae8d4f219d55ee7a20af55edd2f
martinrojasa <martinrojasa@outlook.com>
2023-03-17 15:16:39 -0400

    checkout paypal

--- src/pages/psico/Checkout.tsx
+++ src/pages/psico/Checkout.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import paypalLogo from "../../assets/images/logo-Paypal.png";
+// import PaypalButton from '../../components/PaypalButton';
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { useNavigate } from 'react-router-dom';
 
 function Checkout() {
+    // const PaymentInfo = {
+    //     amount: 90,
+    // };
+
+    const navigate = useNavigate();
+
     return (
-        <div className="flex flex-col items-center mb-10">
+        <div className="flex flex-col items-center mb-10 h-">
             <div className="h-16 flex items-center justify-center font-bold text-4xl mb-4">Checkout</div>
             <div className="w-5/6 h-auto border-gray-300 border-2 rounded-xl shadow-2xl max-w-sm">
                 <div className="h-8 flex items-center justify-center font-medium text-2xl mb-4 mt-4">Información de la cita</div>
@@ -23,18 +32,41 @@ function Checkout() {
                     <span className="font-bold">Total:</span>&nbsp; $90.00
                 </div>
                 <div className="flex flex-col items-center pt-7 justify-center text-center content-center center">
-                    <button className="bg-yellow-400 h-12 w-60 rounded-full hover:scale-105">
-                        <div className="flex justify-center gap-2">
-                            <img src={paypalLogo} alt="paypal-logo" className="h-6" />
-                            <div className="font-bold">Checkout</div>
-                        </div>
-                    </button>
-                    <button className="bg-gray-800 h-12 w-60 rounded-full mt-3 mb-7 hover:scale-105">
-                        <div className="flex justify-center gap-2">
-                            <img src="../../src/assets/icons/paypal.svg" alt="paypal-logo" className="h-6" />
-                            <div className="font-bold text-white">Pay Later</div>
-                        </div>
-                    </button>
+                    <div className="paypal-button_container pb-7 w-4/5">
+                        <PayPalScriptProvider options={{ "client-id": "AQ4F8JZF3wIeaJtUxtimxHhfaVFrgivCZOnffwyp-bfQfqSM_134_d7OO9nUiPwZ_GHZxXg321g4kUWE"}}>
+                            <PayPalButtons 
+                                style={{
+                                    layout: "horizontal",
+                                    shape: "pill",
+                                    label: "checkout",
+                                    height: 50,
+                                }}
+
+                                createOrder={
+                                    (data, actions) => {
+                                        return actions.order.create({
+                                            purchase_units: [
+                                                {
+                                                    amount: {
+                                                        value: "90.0",
+                                                        currency_code: "USD",
+                                                    },
+                                                },
+                                            ],
+                                        });
+                                    }
+                                }
+                                onApprove={
+                                    (data: any, actions: any) => {
+                                        return actions.order?.capture().then(function(details: any) {
+                                            alert("Transaction completed by " + details.payer.name?.given_name);
+                                            navigate("/landing");
+                                        });
+                                    }
+                                }
+                            />
+                        </PayPalScriptProvider>
+                    </div>
                 </div>
             </div>
         </div>
