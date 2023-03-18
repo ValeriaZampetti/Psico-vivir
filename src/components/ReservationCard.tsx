869aaaf006e3c72c8a2f065978194b327339bb07
Sergionx <sergio180203@gmail.com>
2023-03-17 20:51:35 -0400

    Refactored for readbility

--- src/components/ReservationCard.tsx
+++ src/components/ReservationCard.tsx
@@ -1,52 +1,73 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { getUserById } from "../firebase/api";
 import { useTimestampToString } from "../hooks/useTimestampToString";
 import { Appointment } from "../interfaces/Appointment";
 import { Client } from "../interfaces/Client";
 
 interface ReservationCardProps {
-    appointment: Appointment;
-  }
+  appointment: Appointment;
+}
 
 export const ReservationCard = (props: ReservationCardProps) => {
-    const [user, setUser] = useState<Client>();
+  const [user, setUser] = useState<Client>();
 
-    const initializeClient = async () => {
-        const ClientSnapshot = await getUserById(props.appointment.clientId);
-        const client: Client = ClientSnapshot as Client;
-        setUser(client);
-      };
-    
-      useEffect(() => {
-        initializeClient();
-      }, []);
+  const initializeClient = async () => {
+    setUser(await getUserById(props.appointment.clientId));
+  };
 
-    const dateString = useTimestampToString(props.appointment.date.seconds);
+  useEffect(() => {
+    initializeClient();
+  }, []);
 
+  const dateString = useTimestampToString(props.appointment.date.seconds);
 
-    return (
-        <div className="h-48 m-0 flex justify-around items-center smax2:h-auto smax2:flex-col smax2:gap-4 smax2:p-8">
-            <img src="../../src/assets/mock/pic.jpg" alt="profile-pic" className="h-32 aspect-square rounded-full border-rose-400 border-8 smax2:h-52"/>
-            <div className="h-1/2 w-1/5 flex flex-col gap-1 bg-white smax2:w-full">
-                <div className="font-semibold text-3xl relative smax1:font-bold smax1:text-xl smax2:flex smax2:justify-center smax2:text-4xl">{user?.name}</div>
-                <div className="relative left-5 text-lg smax2:inset-x-0 smax2:flex smax2:justify-center smax2:text-3xl">{dateString}</div>
-            </div>
-            <div className="h-2/3 w-2/6 flex flex-col justify-center text-gray-500 smax2:w-full smax2:pb-8">
-                <div className="text-xl font-semibold smax2:text-xl smax2:flex smax2:justify-center">Breve descripción:</div>
-                <div className="smax2:text-sm smax2:flex smax2:text-justify">
-                    {props.appointment.description}
-                </div>
-            </div>
-            <div className="w-1/5 h-1/3 flex justify-center items-center gap-8 smax2:w-48 smax2:h-16">
-                <div className="h-full aspect-square rounded-2xl  border-rose-400 border-4 flex items-center justify-center hover:scale-110">
-                    <img src="../../src/assets/icons/chat.svg" alt="check-icon" className="h-8"/>
-                </div>
-                <div className="h-full aspect-square rounded-2xl  border-rose-400 border-4 flex items-center justify-center hover:scale-110">
-                    <img src="../../src/assets/icons/check.svg" alt="check-icon" className="h-12"/>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <main className="h-48 m-0 flex justify-around items-center max-[900px]:h-auto max-[900px]:flex-col max-[900px]:gap-4 max-[900px]:p-8">
+      <img
+        src="../../src/assets/mock/pic.jpg"
+        alt="profile-pic"
+        className="h-32 aspect-square rounded-full border-rose-400 border-8 smax2:h-52"
+      />
+
+      <header className="h-1/2 w-1/5 flex flex-col gap-1 bg-white max-[900px]:w-full">
+        <h1 className="font-semibold text-3xl max-[1080px]:font-bold max-[1080px]:text-xl max-[900px]:flex max-[900px]:justify-center max-[900px]:text-4xl">
+          {user?.name}
+        </h1>
+
+        <h2 className="text-lg max-[900px]:text-center max-[900px]:text-3xl">
+          {dateString}
+        </h2>
+      </header>
+
+      <section className="h-2/3 w-2/6 flex flex-col justify-center text-gray-500 max-[900px]:w-full max-[900px]:pb-8">
+        <h3 className="text-xl font-semibold max-[900px]:text-xl max-[900px]:flex max-[900px]:justify-center">
+          Breve descripción:
+        </h3>
+
+        <h4 className="max-[900px]:text-sm max-[900px]:flex text-justify justify-center">
+          {props.appointment.description}
+        </h4>
+      </section>
+
+      <footer className="flex flex-wrap justify-center items-center gap-8 w-48 h-16">
+        <button className="h-full aspect-square rounded-2xl  border-rose-400 border-4 flex items-center justify-center hover:scale-110">
+          <img
+            src="../../src/assets/icons/chat.svg"
+            alt="check-icon"
+            className="h-8"
+          />
+        </button>
+
+        <button className="h-full aspect-square rounded-2xl  border-rose-400 border-4 flex items-center justify-center hover:scale-110">
+          <img
+            src="../../src/assets/icons/check.svg"
+            alt="check-icon"
+            className="h-12"
+          />
+        </button>
+      </footer>
+    </main>
+  );
 };
 
 export default ReservationCard;
