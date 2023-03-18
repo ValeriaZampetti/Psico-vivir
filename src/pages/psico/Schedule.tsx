92b83b95be1fa9abb7ca5597ed4e5ff65690e5ab
Sergionx <sergio180203@gmail.com>
2023-03-17 21:00:55 -0400

    inifinite loading

--- src/pages/psico/Schedule.tsx
+++ src/pages/psico/Schedule.tsx
@@ -1,38 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import ReservationCard from '../../components/ReservationCard'
-import { getAppointmentsDoctor } from '../../firebase/api';
-import { Appointment } from '../../interfaces/Appointment';
+import { DocumentData, DocumentSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import ReservationCard from "../../components/ReservationCard";
+import {
+  getAppointmentsDoctor,
+  getAppointmentsDoctorPaginated,
+} from "../../firebase/api";
+import { useAuth } from "../../hooks/useAuth";
+import useInfiniteLoading from "../../hooks/useInfiniteLoading";
+import { Appointment } from "../../interfaces/Appointment";
 
 function Schedule() {
-   const [appointments, setAppointments] = useState<Appointment[]>([]);
+  const { user } = useAuth();
 
-  const initializeAppointments = async () => {
-    const AppointmentsSnapshot = await getAppointmentsDoctor("765GVZowDwloZq07LAfp");
+  const {
+    items,
+    lastItemRef,
+  }: {
+    items: Appointment[];
+    loadItems: () => Promise<DocumentSnapshot<DocumentData>[]>;
+    lastItemRef: (node: any) => void;
+  } = useInfiniteLoading({
+    getItemsWithId: getAppointmentsDoctorPaginated,
+    // idToPass: user?.email
+    idToPass: "765GVZowDwloZq07LAfp",
+  });
 
-    const appointments: Appointment[] = [];
-    AppointmentsSnapshot.forEach((Appointment) => {
-      appointments.push(Appointment)
-    });
-    
-    setAppointments(appointments);
-  };
+  const cards = items.map((appointment, index) => {
+    if (index === items.length - 1) {
+      return (
+        <li
+          className="border-b-2 border-rose-300 last:border-b-0 last:mb-2"
+          key={appointment.id}
+          ref={lastItemRef}
+        >
+          <ReservationCard appointment={appointment} />
+        </li>
+      );
+    }
 
-  useEffect(() => {
-    initializeAppointments();
-  }, []);
+    return (
+      <li
+        className="border-b-2 border-rose-300 last:border-b-0 last:mb-2"
+        key={appointment.id}
+      >
+        <ReservationCard appointment={appointment} />
+      </li>
+    );
+  });
 
+  // TODO - Algo cuando no hay citas agendadas
   return (
     <div>
       <div className="h-20 flex items-center smax1:h-16">
-        <div className="h-12 relative left-16 flex items-center font-bold text-4xl smax2:left-6">Citas agendadas</div>
+        <div className="h-12 relative left-16 flex items-center font-bold text-4xl smax2:left-6">
+          Citas agendadas
+        </div>
       </div>
-      <ul>
-        {appointments.map(appointment => (
-          <li className="border-b-2 border-rose-300 last:border-b-0 last:mb-2" key={appointment.id}><ReservationCard appointment={appointment}/></li>
-        ))}
-      </ul>
+
+      <ul>{cards}</ul>
     </div>
-  )
+  );
 }
 
-export default Schedule
+export default Schedule;
