545eb663e0ea355ec1a985b206f73fd1642f34d2
Sergionx <sergio180203@gmail.com>
2023-03-19 14:08:52 -0400

    preliminal chat view

--- src/pages/psico/Chat.tsx
+++ src/pages/psico/Chat.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import ChatContainer from "../../components/chat/ChatContainer";
+import ChatSideBar from "../../components/chat/ChatSideBar";
+import InputMessage from "../../components/chat/InputMessage";
 
+// FIXME - Arreglar todos los errores de responsive
 function Chat() {
   return (
-    <div>Chat</div>
-  )
+    <div className="bg-primary-normal h-screen w-screen flex items-center justify-center">
+      <main className="rounded-xl border-2 border-white w-[90%] md:w-[65%] h-[80%] overflow-hidden">
+        <div className="flex overflow-hidden h-[calc(100%_-_80px)]">
+          <ChatSideBar />
+          <ChatContainer />
+        </div>
+
+        <InputMessage />
+      </main>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
