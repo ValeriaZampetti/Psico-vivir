8ebba198d691e404626cb8ca8eb05fb733a3509a
Rodolfo M <rodolfomarin2020@gmail.com>
2023-03-19 13:13:28 -0400

    vista responsive

--- src/pages/psico/WriteReview.tsx
+++ src/pages/psico/WriteReview.tsx
@@ -7,8 +7,8 @@ function WriteReview() {
   const [rating, setRating] = useState(0);
 
   return (
-    <div>
-      <div className="h-16 p-5 relative top-8 left-7 font-bold text-4xl flex items-center text-center border-solid drop-shadow-md mb-10">
+    <div className="p-5 lg:p-10">
+      <div className="relative flex items-center text-center text-3xl md:text-4xl lg:text-5xl font-bold border-solid drop-shadow-md mb-8 md:mb-10">
         Califique su experiencia
       </div>
 
@@ -16,24 +16,24 @@ function WriteReview() {
         <img
           src={Profile}
           alt="doctor-profile-pic"
-          className="h-48 rounded-full border-rose-300 border-8"
+          className="h-40 md:h-48 lg:h-64 rounded-full border-rose-300 border-8 mb-4 md:mb-6"
         />
 
-        <div>
-          <div className="text-3xl font-medium mt-5 border-solid drop-shadow-md">
-            Dr. Juan Perez
+        <div className="text-2xl md:text-3xl lg:text-4xl font-medium text-center border-solid drop-shadow-md mb-2 md:mb-4">
+          Dr. Juan Perez
+        </div>
+
+        <div className="flex flex-row items-center gap-4 md:gap-6">
+          <div className="flex items-center">
+            <StarRating
+              currentRating={rating}
+              handleCurrentRating={(newRating: number) => setRating(newRating)}
+              svgClass="h-7 md:h-9 w-10 md:w-12"
+            />
           </div>
         </div>
 
-        <div className="position-absolute p-5 left-264 top-366 flex flex-row items-start gap-10">
-          <StarRating
-            currentRating={rating}
-            handleCurrentRating={(newRating: number) => setRating(newRating)}
-            svgClass="h-9 w-12"
-          />
-        </div>
-
-        <div className="p-2 font-bold mt-5 border-solid drop-shadow-md">
+        <div className="text-xl md:text-2xl font-bold mt-6 border-solid drop-shadow-md">
           Comentarios
         </div>
 
@@ -41,13 +41,15 @@ function WriteReview() {
           name=""
           id=""
           placeholder="Escribe tu comentario aqui..."
-          className="rounded-lg px-4 resize-none h-20 w-1/2 border-2 border-rose-300 outline-none"
+          className="rounded-lg px-4 resize-none h-24 md:h-32 w-full md:w-2/3 border-2 border-rose-300 outline-none mt-4 md:mt-6"
         ></textarea>
 
-        <button type="button" className="bg-rose-300 rounded-lg px-4 py-2 mt-5 text-white font-bold w-44 text-2xl">
+        <button
+          type="button"
+          className="bg-rose-300 rounded-lg px-4 py-2 mt-8 md:mt-10 text-white font-bold w-full md:w-auto text-xl md:text-2xl"
+        >
           Enviar
         </button>
-
       </div>
     </div>
   );
