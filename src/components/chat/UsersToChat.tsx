00e97de6f93c112e0bef2059329ba43aa9f4f336
Sergionx <sergio180203@gmail.com>
2023-03-26 22:52:24 -0400

    better currentChat UI

--- src/components/chat/UsersToChat.tsx
+++ src/components/chat/UsersToChat.tsx
@@ -11,16 +11,16 @@ function UsersToChat(props: IProps) {
 
   // TODO - Si no hay usuarios, poner un mensaje de que no hay usuarios
   // TODO - Si es el mismo usuario que esta seleccionado, ponerle un borde, background o algo
-  const usersTochat = props.users.map((user, index) => {    
+  const usersTochat = props.users.map((user, index) => {
     return (
       <section
         key={user.id}
         onClick={() => handleSelectUserToChat(user)}
-        className={`flex flex-col border-b-2 border-gray-400 gap-y-2 w-full px-4 py-2
+        className={`flex flex-col  gap-y-2 w-full px-4 py-2
           ${
-            user.id === (currentUserToChat?.id ?? "")
-              ? "bg-secondary-strong"
-              : ""
+            user.id === (currentUserToChat?.id ?? "") ?
+            "bg-secondary-strong  border-black" :
+            "border-gray-400 bg-[#F5F5F5]"
           } 
           cursor-pointer hover:bg-secondary-strong duration-100`}
       >
