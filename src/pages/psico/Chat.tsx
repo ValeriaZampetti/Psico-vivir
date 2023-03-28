1944783cf7bc70790140e14b0820a74b72f90541
Sergionx <sergio180203@gmail.com>
2023-03-28 13:20:46 -0400

    h-[80%] -> h-[90%]

--- src/pages/psico/Chat.tsx
+++ src/pages/psico/Chat.tsx
@@ -9,7 +9,7 @@ function Chat() {
   return (
     <ChatProvider>
       <div className="bg-[#f9f0f8] h-screen w-screen flex items-center justify-center">
-        <main className="rounded-xl border-2 border-primary-normal w-[90%] md:w-[65%] h-[80%] overflow-hidden">
+        <main className="rounded-xl border-2 border-primary-normal w-[90%] md:w-[65%] h-[90%] overflow-hidden">
           <div className="flex overflow-hidden h-[calc(100%_-_80px)]">
             <ChatSideBar />
             <ChatContainer />
